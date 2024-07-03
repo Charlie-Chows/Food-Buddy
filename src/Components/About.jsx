99bd2158318a9de211e14e0a163bf2e800e6db0b
Charlie-Chows <sujithkasthuri247@gmail.com>
2024-07-03 19:21:01 +0530

    write class based components on About section

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,7 +1,20 @@
-const About = () => {
-    return (
-        <div> About </div>
-    )
+import User from "./User";
+import { Component } from "react";
+
+class About extends Component {
+    constructor ( props ) {
+        super ( props );
+    }
+    
+    render() {
+        return (
+            <div> 
+                <h1> About Page </h1>
+                <User name = {"Sujith Kumar Chowdary"} />
+            </div>
+    
+        )
+    }
 }
 
 export default About;
---
+++ src/Components/User.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+class User extends React.Component {
+
+    constructor ( props ) {
+        super ( props );
+        this.state = {
+            userInfo : {
+                name : "Dummy Name",
+                location : "Default",
+                avatar_url : "https://DummyUrl...."
+            }
+        }
+    }
+
+    async componentDidMount ( ) {
+        const data = await fetch ("https://api.github.com/users/Charlie-Chows");
+        const json = await data.json();
+        this.setState ( {
+            userInfo : json,
+        });
+    }
+
+    render () {
+        const { name, public_repos, avatar_url } = this.state.userInfo;
+        
+        return (
+            <div className = "user-card">
+                <img className = "avatar" src = { avatar_url } />
+                <h2> Name : { name } </h2>
+                <h3> Num of Github Repositaries : { public_repos } </h3>
+                <h4> Mail : abc@gmail.com </h4>
+            </div>
+        )
+    }
+}
+
+export default User;
